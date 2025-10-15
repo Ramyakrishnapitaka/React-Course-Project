6ea50628ddc0c2639dea7d4bfcb5fa340ee50208
Ramyakrishnapitaka <154577647+Ramyakrishnapitaka@users.noreply.github.com>
2025-10-15 23:40:20 +0530

    Remove comments for course cards in home component
    Removed comments for course cards to clean up the code.

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -25,7 +25,6 @@ function Home() {
       <section className="container my-4">
         <h5 className="mb-3 popular-title text-center">Popular Courses</h5>
         <div className="d-flex justify-content-center gap-3 course-cards">
-          {/* Course Card 1 */}
           <div className="card course-card p-3 text-center" style={{width:"18rem"}}>
             <div className="icon-bg react-bg mx-auto mb-3">
               <img src={react} alt="React Basics" className="icon-image"/>
@@ -34,8 +33,6 @@ function Home() {
             <p className="course-subtitle">Frontend</p>
             <button className="btn btn-success btn-sm enroll-btn" onClick={gocourse}>View Details</button>
           </div>
-
-          {/* Course Card 2 */}
           <div className="card course-card p-3 text-center" style={{width:"18rem"}}>
             <div className="icon-bg node-bg mx-auto mb-3">
               <img src={node} alt="Intro to Python" className="icon-image"/>
@@ -44,8 +41,6 @@ function Home() {
             <p className="course-subtitle">Backend</p>
             <button className="btn btn-success btn-sm enroll-btn" onClick={gocourse}>View Details</button>
           </div>
-
-          {/* Course Card 3 */}
           <div className="card course-card p-3 text-center" style={{width:"18rem"}}>
             <div className="icon-bg design-bg mx-auto mb-3">
               <img src={mongodb} alt="UI/UX Design" className="icon-image"/>
